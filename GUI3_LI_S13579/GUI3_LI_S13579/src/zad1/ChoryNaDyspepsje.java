87666ca21bee0ce29d6d5cb9f52bb4f8e23fdadd
IlyaLoshchinin <ilyapje@gmail.com>
2016-03-15 19:00:52 +0100

    -

---
+++ GUI3_LI_S13579/GUI3_LI_S13579/src/zad1/ChoryNaDyspepsje.java
@@ -0,0 +1,11 @@
+package zad1;
+
+public class ChoryNaDyspepsje extends Pacjent{
+	
+	public ChoryNaDyspepsje(String imie){
+		super.setImie(imie);
+		super.setNazwaChoroby("dyspepsja");
+		super.setZastosowano("wegiel");
+	}
+	
+}
---
+++ GUI3_LI_S13579/GUI3_LI_S13579/src/zad1/ChoryNaNoge.java
@@ -0,0 +1,12 @@
+package zad1;
+
+public class ChoryNaNoge extends Pacjent {
+	
+	public ChoryNaNoge(String imie){
+		super.setImie(imie);
+		super.setNazwaChoroby("noga");
+		super.setZastosowano("gips");
+		}
+	
+	
+}
